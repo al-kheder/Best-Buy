b97df94c115627a81d53e20e8434392653f2c4ea
ali al-kheder <alkhdyyr@gmail.com>
2025-02-12 16:26:33 +0100

    create product class

--- products.py
+++ products.py
@@ -1,1 +1,35 @@
 class Product:
+    def __init__(self, name:str, price:float,quantity:int):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+
+    #getter method
+    def get_quantity(self)->int:
+        return self.quantity
+
+    #setter method
+    def set_quantity(self, quantity:int):
+        self.quantity = quantity
+
+    def is_active(self)->bool:
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactive(self)->bool:
+        self.active = False
+
+    def show(self) ->str:
+        print(f"Name: {self.name}, Price: {self.price}, Quantity: {self.quantity}, Active: {self.active}")
+
+    def buy(self, quantity:int) -> float:
+        if self.quantity >= quantity:
+            self.quantity -= quantity
+            return self.price * quantity
+        else:
+            print("Not enough quantity")
+
