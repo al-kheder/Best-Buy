9f2179a462cca3fd7b1bd7e5b82d7006420784b4
ali al-kheder <alkhdyyr@gmail.com>
2025-02-12 17:01:43 +0100

    create the start method

--- main.py
+++ main.py
@@ -15,3 +15,28 @@ best_buy = Store(product_list)
 products = best_buy.get_all_products()
 print(best_buy.get_total_quantity())
 print(best_buy.order([(products[0], 1), (products[1], 2)]))
+
+def start(store):
+    while True:
+        print("""
+        1. List all products in store 
+        2. Show total amount in store
+        3. Make an order
+        4.Quit
+        """)
+        actions = {
+            1: store.get_all_products,
+            2: store.get_total_quantity,
+            3: store.order,
+            4: Quit
+        }
+        choice = int(input("Enter your choice: "))
+        if choice == 4:
+            print("Goodbye!")
+            break
+        elif choice in actions:
+            actions[choice]()
+        else:
+            print("Invalid choice")
+
+
